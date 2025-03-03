834af04a762967001041d54a4e848f65628214a5
Anurag Singh <141731191+Anurag5080@users.noreply.github.com>
2025-03-03 18:13:39 +0530

    Update Promises.js

--- AdvanceJS/Promises.js
+++ AdvanceJS/Promises.js
@@ -1,8 +1,6 @@
 const promiseOne = new Promise(function(resolve, reject){
     //Do an async task
     // DB calls, cryptography, network
-
-    
     setTimeout(function(){
         console.log('Async task is compelete');
         resolve()
